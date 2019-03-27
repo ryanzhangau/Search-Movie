ef58da6b0dfcc01221cdaa91b4fbf0eb7718e536
Ryan Zhang <ryanzhangau@gmail.com>
2019-03-28 08:23:29 +1100

    add thunk for aync actions

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import movieReducer from "./reducers/movieReducer";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 const store = createStore(
   combineReducers({
     movieReducer
   }),
   {},
-  applyMiddleware(logger)
+  applyMiddleware(logger, thunk)
 );
 
 export default store;
