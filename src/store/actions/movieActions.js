9401486036a04bde4a7e0587a99079a279553217
Ryan Zhang <ryanzhangau@gmail.com>
2019-03-28 08:26:06 +1100

    fixed for typo

--- src/store/actions/movieActions.js
+++ src/store/actions/movieActions.js
@@ -1,41 +1,43 @@
 import axios from "axios";
-import { watch } from "chokidar";
-require("dotenv").config();
 
-const APIKey = process.env.APIKey;
-const watchlist = localStorage.getItem("watchlist") || "[]";
+const getWatchlist = () =>
+  JSON.parse(localStorage.getItem("watchlist") || "[]");
+
+const API_KEY = "31169fa";
 
 export const searchMovie = query => dispatch => {
   axios
-    .get(`https://www.omdbapi.com/?apikey=${APIKey}&s=${query}`)
+    .get(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`)
     .then(res => {
       dispatch({
-        type: "SEARCH_MOIVE",
-        payload: res.data
+        type: "SEARCH_MOVIE",
+        payload: res.data.Search
       });
     });
 };
 
 export const fetchWatchlist = () => dispatch => {
-  dispatch({
-    type: "FETCH_MOIVE",
-    payload: JSON.parse(watchlist)
-  });
+  const watchlsit = getWatchlist();
+  dispatch({ type: "FETCH_WATCHLIST", payload: watchlsit });
 };
 
 export const addMovie = movie => dispatch => {
-  const m = JSON.parse(watchlist).filter(m => m.imdbID === movie.imdbID);
-  if (m.length === 0)
+  const watchlist = getWatchlist();
+  const m = watchlist.filter(m => m.imdbID === movie.imdbID);
+  if (m.length === 0) {
+    localStorage.setItem("watchlist", JSON.stringify([...watchlist, movie]));
     dispatch({
-      type: "ADD_MOIVE",
+      type: "ADD_MOVIE",
       payload: movie
     });
+  }
 };
 
-export const removeMoive = movie => dispatch => {
-  const movies = JSON.parse(watchlist).map(m => m.imdbID !== movie.imdbID);
+export const removeMovie = movie => dispatch => {
+  const movies = getWatchlist().filter(m => m.imdbID !== movie.imdbID);
+  localStorage.setItem("watchlist", JSON.stringify(movies));
   dispatch({
-    type: "REMOVE_MOIVE",
+    type: "REMOVE_MOVIE",
     payload: movies
   });
 };
