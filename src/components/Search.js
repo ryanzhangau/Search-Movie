182e5b8838753b96ed4b70dd7285d977ae40cfc1
Ryan Zhang <ryanzhangau@gmail.com>
2019-03-28 08:28:05 +1100

    load search style

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { searchMovie } from "../store/actions/movieActions";
+import "../styles/search.scss";
 
 class Search extends Component {
   constructor(props) {
