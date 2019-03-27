21c984431d5a18351b06c54fcaa5e5c76e8c6177
Ryan Zhang <ryanzhangau@gmail.com>
2019-03-28 09:49:10 +1100

    add header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import Home from "./views/Home";
 import WatchList from "./views/WatchList";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Route exact path="/" component={Home} />
         <Route exact path="/watchlist" component={WatchList} />
       </div>
