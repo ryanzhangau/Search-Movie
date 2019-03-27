655c5d91e1de41c471f8c6bd71eca477b51aae32
Ryan Zhang <ryanzhangau@gmail.com>
2019-03-28 08:24:12 +1100

    add Route for landing page and watchlist page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./views/Home";
+import WatchList from "./views/WatchList";
 
 class App extends Component {
   render() {
-    return <div className="App">code goes here</div>;
+    return (
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/watchlist" component={WatchList} />
+      </div>
+    );
   }
 }
 
